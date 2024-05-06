528b8bbe89d335c2b2cd2948bb40de42e41c645e
Borys-Grinberg <borys.grinberg@gmail.com>
2024-05-06 23:02:08 +0200

    browser router added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
+import React, { Suspense, lazy } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+
+const Home = lazy(() => import('./components/Home'));
+const Movies = lazy(() => import('./components/Movies'));
+const MovieDetails = lazy(() => import('./components/MovieDetails'));
+const Cast = lazy(() => import('./components/Cast'));
+const Reviews = lazy(() => import('./components/Reviews'));
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/movies" exact component={Movies} />
+          <Route path="/movies/:movieId" exact component={MovieDetails} />
+          <Route path="/movies/:movieId/cast" exact component={Cast} />
+          <Route path="/movies/:movieId/reviews" exact component={Reviews} />
+          <Redirect to="/" />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 };
+
+export default App;
