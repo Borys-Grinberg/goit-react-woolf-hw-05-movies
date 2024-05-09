6e9f93b5e7fef1af834894de91452b360b778959
Borys-Grinberg <borys.grinberg@gmail.com>
2024-05-09 23:08:08 +0200

    MoviesList edit

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,13 +1,21 @@
-// src/components/MoviesList/MoviesList.jsx
-import React from 'react';
+import css from './MoviesList.module.css';
+import { Link, useLocation } from 'react-router-dom';
 
 const MoviesList = ({ movies }) => {
+  const location = useLocation();
+
   return (
-    <ul>
-      {movies.map(movie => (
-        <li key={movie.id}>{movie.title}</li>
-      ))}
-    </ul>
+    <div className={css.container}>
+      <ul className={css.list}>
+        {movies.map(movie => (
+          <li key={movie.id} className={css.item}>
+            <Link to={`/movies/${movie.id}`} state={{ from: location }}>
+              {movie.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
